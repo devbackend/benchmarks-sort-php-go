cbd56f0cc2b3eba171623cc776c39c8e9dbfb7a8
Krivonos Ivan <devbackend@yandex.ru>
2019-10-31 19:23:55 +0300

    Добавлена нативная сортировка

--- README.md
+++ README.md
@@ -5,3 +5,25 @@ go test -bench . *.go
 ```
 php sort.php
 ```
+
+```
+// Golang quickSort
+
+goos: darwin
+goarch: amd64
+BenchmarkQuickSort1000-4       	   19274	     73380 ns/op
+BenchmarkQuickSort10000-4      	    2436	    478411 ns/op
+BenchmarkQuickSort100000-4     	     222	   5187577 ns/op
+BenchmarkQuickSort1000000-4    	      22	  53431253 ns/op
+BenchmarkQuickSort10000000-4   	       2	 574170556 ns/op
+```
+
+```
+// PHP quickSort
+
+./nums_1000:     total = 0.01116s;   average = 0.00558s
+./nums_10000:    total = 0.11796s;   average = 0.05898s
+./nums_100000:   total = 1.23729s;   average = 0.61865s
+./nums_1000000:  total = 16.03092s;  average = 8.01546s
+./nums_10000000: total = 169.25291s; average = 84.62646s
+```
--- sort.php
+++ sort.php
@@ -25,7 +25,7 @@ function quickSort(array $items): array {
     return $items;
 }
 
-function sortFile(string $path) {
+function sortFile(string $path, bool $native = false) {
     $h = fopen($path, 'r');
     $nums = [];
     while (true) {
@@ -41,24 +41,36 @@ function sortFile(string $path) {
 
     fclose($h);
 
-    quickSort($nums);
+    if (!$native) {
+        quickSort($nums);
+
+        return;
+    }
+
+    sort($nums);
 }
 
 define('ITERS', 2);
 
-function benchTest(string $file) {
+function benchTest(string $file, bool $native) {
     $start = microtime(true);
     for ($i = 0; $i < ITERS; $i++) {
-        sortFile($file);
+        sortFile($file, $native);
     }
     $total = round(microtime(true) - $start, 5);
     $avg = round($total / ITERS, 5);
 
-    return $file . ': total = ' . $total . 's; average = ' . $avg . 's';
+    return ($native ? 'native sort' : 'quickSort') . ' for ' . $file . ': total = ' . $total . 's; average = ' . $avg . 's';
 }
 
-echo benchTest('./nums_1000') . PHP_EOL;
-echo benchTest('./nums_10000') . PHP_EOL;
-echo benchTest('./nums_100000') . PHP_EOL;
-echo benchTest('./nums_1000000') . PHP_EOL;
-echo benchTest('./nums_10000000') . PHP_EOL;
+echo benchTest('./nums_1000', false) . PHP_EOL;
+echo benchTest('./nums_10000', false) . PHP_EOL;
+echo benchTest('./nums_100000', false) . PHP_EOL;
+echo benchTest('./nums_1000000', false) . PHP_EOL;
+echo benchTest('./nums_10000000', false) . PHP_EOL;
+
+echo benchTest('./nums_1000', true) . PHP_EOL;
+echo benchTest('./nums_10000', true) . PHP_EOL;
+echo benchTest('./nums_100000', true) . PHP_EOL;
+echo benchTest('./nums_1000000', true) . PHP_EOL;
+echo benchTest('./nums_10000000', true) . PHP_EOL;
